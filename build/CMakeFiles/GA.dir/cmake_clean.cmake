dd9f2e8b1e3226198b0e81cc35acd994b90ed37d
Xianyu <fanghejin@qq.com>
2021-07-14 20:33:00 +0800

    构建文件

---
+++ build/CMakeFiles/GA.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../bin/libGA.dll"
+  "../bin/libGA.dll.a"
+  "../bin/libGA.dll.manifest"
+  "../bin/libGA.pdb"
+  "CMakeFiles/GA.dir/src/Chromosome.cpp.obj"
+  "CMakeFiles/GA.dir/src/GeneticAlgorithm.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GA.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
