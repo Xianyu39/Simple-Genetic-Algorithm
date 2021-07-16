719377d54fe11fb136a3a4460ee370d2a673cf15
Xianyu <fanghejin@qq.com>
2021-07-16 20:33:27 +0800

    测试程序更新

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     GA ga(20);
     // ga.test();
-    auto winner = ga.evolution(1000000);
+    auto winner = ga.evolution(100);
     cout << winner.g1 << ":" << winner.g2 << endl;
     cout << "Run successfully." << endl;
     return 0;
